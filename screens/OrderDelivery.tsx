02129e9681d1d0024f9c852f25e69a625ccaf5c3
Fatih <h.fatih_@outlook.com>
2022-05-11 14:41:01 +0300

    android icin google maps

--- screens/OrderDelivery.tsx
+++ screens/OrderDelivery.tsx
@@ -1,12 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import MapView from 'react-native-maps';
 
 export default function OrderDelivery() {
   return (
     <View style={styles.container}>
-      <View>
-
-      </View>
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     </View>
   )
 }
